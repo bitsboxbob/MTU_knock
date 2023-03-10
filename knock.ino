27262ead42e99509ef90c6a85221b173d02399aa
bitsboxbob <36841297+bitsboxbob@users.noreply.github.com>
2023-03-10 13:30:18 +0000

    Add files via upload

---
+++ knock.ino
@@ -0,0 +1,49 @@
+/*
+Toggle a LED with a tap on a piezo transducer
+*/
+
+#define piezo A2
+#define LED A5
+
+int knockValue = 0; //stores the value read from the piezo input
+int threshold = 50;  //the value above which the knockValue must reach
+bool ledOn = 0; //led status flag
+
+void readKnock();
+
+void setup() 
+{
+  Serial.begin(9600);     //  opens serial port, sets data rate to 9600 bps
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED,HIGH);
+  delay(2000);
+  digitalWrite(LED,LOW);
+  delay(500);
+}
+
+void loop() 
+{
+  readKnock();
+  //Serial.print("KnockValue= ");
+  //Serial.println(knockValue);
+
+  if (ledOn)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+    digitalWrite(LED,LOW);
+  }
+}
+
+void readKnock()
+{
+  knockValue = analogRead(piezo);
+  if (knockValue >= threshold)
+  {
+    ledOn = !ledOn;
+    delay(500);
+  }
+  
+}
